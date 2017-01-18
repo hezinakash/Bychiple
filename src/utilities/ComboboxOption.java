508db6c293404a80960d9c854378a08dbc32eb7f
Hezi Nakash <hezi.hn@gmail.com>
2017-01-18 23:34:06 +0200

    Create ComboboxOption.java

---
+++ src/utilities/ComboboxOption.java
@@ -0,0 +1,14 @@
+package utilities;
+
+public class ComboboxOption 
+{
+	@SuppressWarnings("unused")
+	private String text;
+	@SuppressWarnings("unused")
+	private String value;
+	
+	public ComboboxOption(String text, String value) {
+		this.text = text;
+		this.value = value;
+	}
+}
